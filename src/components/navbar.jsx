b3da0119cb340e6bff0bce74f43a40ccbc5a68e3
Silverius Calvin <silveriuscalvin35@gmail.com>
2025-05-22 03:19:57 +0700

    created navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,48 @@
+import MOLogo from "../assets/linen_logo.svg";
+import forumIcon from "../assets/forum_icon.svg"
+import leaderboardIcon from "../assets/leaderboard_icon.svg"
+import learnIcon from "../assets/learn_icon.svg"
+import notifIcon from "../assets/notification_icon.svg"
+import profileIcon from "../assets/profile_icon.svg"
+import settingIcon from "../assets/setting_icon.svg"
+import "../css/navbar.css";
+
+function navbar() {
+  const nav_item = [
+    {id: 1, icon: learnIcon, name: "Learn"},
+    {id: 2, icon: leaderboardIcon, name: "Leaderboard"},
+    {id: 3, icon: forumIcon, name: "Forum"},
+    {id: 4, icon: notifIcon, name: "Notification"},
+    {id: 5, icon: profileIcon, name: "Profile"},
+    {id: 6, icon: settingIcon, name: "Setting"},
+  ];
+
+  return (
+    <div className="nav">
+      <div className="top">
+        <div className="logo-title">
+          <img src={MOLogo} alt="MathOLearn Logo" />
+          <h1 id="navbarTitle">MathOLearn</h1>
+        </div>
+        <div className="nav-list">
+          {nav_item.slice(0, 3).map((item) => (
+            <div key={item.id} className="nav-item">
+              <img src={item.icon} alt={item.name} className="nav-icon" />
+              <span className="nav-name" id="navbarText">{item.name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="nav-list">
+        {nav_item.slice(3, 6).map((item) => (
+          <div key={item.id} className="nav-item">
+            <img src={item.icon} alt={item.name} className="nav-icon" />
+            <span className="nav-name"id="navbarText">{item.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default navbar;
