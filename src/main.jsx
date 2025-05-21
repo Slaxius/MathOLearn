a77b252a94f6ac7e964e97122841da2ea4682ccc
Silverius Calvin <silveriuscalvin35@gmail.com>
2025-05-22 03:36:31 +0700

    Rename web name to MathOLearn

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import App from './App.jsx'
-import Navbar from './components/navbar.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./css/index.css";
+import App from "./App.jsx";
+import Navbar from "./components/navbar.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <Navbar /> */}
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
