c22aa0256136581c30a838aac87a639169128184
Silverius Calvin <silveriuscalvin35@gmail.com>
2025-05-22 03:28:49 +0700

    updated App page

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
-// import App from './App.jsx'
+import App from './App.jsx'
 import Navbar from './components/navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
-    {/* <App /> */}
+    {/* <Navbar /> */}
+    <App />
   </StrictMode>,
 )
