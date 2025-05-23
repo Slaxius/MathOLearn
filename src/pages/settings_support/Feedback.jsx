1c5bc230b9d6ce403ad59356a0ebd3e61e2192a1
Silverius Calvin <silveriuscalvin35@gmail.com>
2025-05-23 22:00:15 +0700

    Added Settings sub-page

---
+++ src/pages/settings_support/Feedback.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Feedback() {
+  return (
+    <div className="page">
+      <Navbar />
+      <Header />
+      <div className="main-section">
+        <p>test</p>
+      </div>
+    </div>
+  );
+}
+
+export default Feedback;
