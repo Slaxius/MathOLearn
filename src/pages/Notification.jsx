de9f25120077a33a9160f66eae7e35c68dc4c98a
Silverius Calvin <silveriuscalvin35@gmail.com>
2025-05-30 03:20:34 +0700

    Created notification page

--- src/pages/Notification.jsx
+++ src/pages/Notification.jsx
@@ -1,15 +1,29 @@
-import React from "react";
 import Navbar from "../components/navbar.jsx";
 import Header from "../components/header.jsx";
 import "../css/Notification.css";
+import NotificationDetail from "../json/notification.json";
 
 function Notification() {
   return (
     <div className="page">
       <Navbar />
       <Header />
-      <div className="main-section">
-        <p>test</p>
+      <div className="main-section notification">
+        {NotificationDetail.map((post, index) => (
+          <div key={index} className={`notification-card ${!post.status ? "opened" : ""}`}>
+            <div className="notif-type">
+              <h1 className="header4">{post.type}</h1>
+            </div>
+            <div className="notif-detail body1">
+              <span className="notif-subject">
+                <p>[ {post.subject} ]</p>
+              </span>
+              <span className="notif-message">
+                <p> {post.message} </p>
+              </span>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
