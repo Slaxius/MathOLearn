c8a3ee37dd30cf2b7a983a1c711118237ddefbb2
Silverius Calvin <silveriuscalvin35@gmail.com>
2025-06-01 23:01:35 +0700

    Created learn's main page

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,16 +1,23 @@
 import Navbar from "../components/navbar.jsx";
 import Header from "../components/header.jsx";
 import "../css/Learn.css";
-import Calendar from "../components/calendar.jsx"
+import Calendar from "../components/calendar.jsx";
+import Subject from "../json/subject.json";
 
 function Learn() {
   return (
     <div className="page">
       <Navbar />
       <Header />
-      <div className="main-section">
-        {/* code */}
-        <Calendar />
+      <Calendar />
+      <div className="main-section learn">
+        <div className="subject-section">
+          {Subject.map((sub, idx) => (
+            <button key={idx} value={sub.subject} className="subject-bubble header5">
+              {sub.subject}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
